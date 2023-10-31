767cd1568e082d4ac60e4d2163a4b41bfdf17b24
victoralv3s <victorpereira8889@gmail.com>
2023-10-31 14:34:53 -0300

    :sparkles: Updated the Controller to test the pizza model

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,10 +1,27 @@
+import Pizza from '../models/Pizza';
+
 // Define the home controller.
 class HomeController {
-	index(req, res) {
-		res.json({message: 'This is the home page'});
+	async index(req, res) {
+		const newPizza = await Pizza.create({
+			name: '4 queijos',
+			description:
+				'Receita de Pizza 4 Queijos deliciosa e cremosa feita com farinha de trigo, fermento em pó, molho de tomate, muçarela, provolone, gorgonzola e parmesão ralado',
+			price: 35.5,
+			size: 'Big',
+			category: 'Creamy',
+			ingredients:
+				'farinha de trigo, fermento em pó, molho de tomate, muçarela, provolone, gorgonzola e parmesão ralado',
+			prep_time: 50,
+			reviews:
+				'Wood oven, nice atmosphere and great pizzas and entrees, as wel as good wine list. Recomendo options for pizza with artichokes and the one with goat cheese',
+			image:
+				'https://img.freepik.com/fotos-gratis/close-up-em-uma-deliciosa-pizza_23-2150852113.jpg?size=626&ext=jpg&ga=GA1.1.1340339033.1694894005&semt=sph',
+			availability: true,
+		});
+		return res.json(newPizza);
 	}
 }
 
 // Export the home controller.
 export default new HomeController();
-
